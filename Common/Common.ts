cc694066dd690cc680da838cf575905b648201b0
isridharan <ishwaryasridharan@gmail.com>
2021-10-04 15:10:04 +1100

    1 file modified

--- Common/Common.ts
+++ Common/Common.ts
@@ -4,8 +4,15 @@ export enum MessageType {
   warning = 'warning',
 }
 
+const regexAlphabetOnly = /^[a-z]+$/i;
+const regexNumberOnly = /^[0-9\b]+$/;
+
 export const checkForAlphabets = function (value) {
-  return /^[a-z]+$/i.test(value);
+  return regexAlphabetOnly.test(value);
+};
+
+export const checkForNumber = function (value) {
+  return regexNumberOnly.test(value);
 };
 
 export const formatCurrency = (amount: number, currency: string) =>
