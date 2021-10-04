9b566df79ee8d62b44b52191accc2ca134c2da8f
isridharan <ishwaryasridharan@gmail.com>
2021-10-04 16:41:37 +1100

    1 file modified

--- Common/Common.ts
+++ Common/Common.ts
@@ -4,17 +4,6 @@ export enum MessageType {
   warning = 'warning',
 }
 
-const regexAlphabetOnly = /^[a-z]+$/i;
-const regexNumberOnly = /^[0-9\b]+$/;
-
-export const checkForAlphabets = function (value) {
-  return regexAlphabetOnly.test(value);
-};
-
-export const checkForNumber = function (value) {
-  return regexNumberOnly.test(value);
-};
-
 export const formatCurrency = (amount: number, currency: string) =>
   new Intl.NumberFormat('en-IN', {
     style: 'currency',
